40c83595c21c698e9beee40959c1d2595636db59
Peter Tembo <simbatembo6@gmail.com>
2024-10-05 23:22:52 +0200

    Saved

--- branches/views.py
+++ branches/views.py
@@ -11,3 +11,26 @@ def branch_add(request):
     else:
         form = BranchForm()
     return render(request, 'branches/branch_form.html', {'form': form})
+
+def branch_update(request, pk):
+    branch = get_object_or_404(Branch, id=pk)
+    if request.method == 'POST':
+        form = BranchForm(request.POST, instance=branch)
+        if form.is_valid():
+            form.save()
+            return redirect('branch_list')
+    else:
+        form = BranchForm(request.POST, instance=branch)
+    return render(request, 'branches/branch_form.html', {'form': form}) 
+
+def branch_delete(request, pk):
+    branch = get_object_or_404(Branch, id=pk)
+    if request.method == 'POST':
+        branch.delete()
+        return redirect('branch_list')
+    return render(request, 'delete_record.html', {'form': form}) 
+        
+
+def branch_list(request):
+    branches = Branch.objects.all()
+    return render(request, 'branches/branch_form.html', {'branches': branches}) 
